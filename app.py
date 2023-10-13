b4223b399b181aba4d4814f1b03504044c45df90
kingtomi <ayodabooluwatomisin@gmail.com>
2023-10-13 09:16:55 +0100

    fixing initial running

--- app.py
+++ app.py
@@ -18,21 +18,28 @@ def run_metrics(metric: str):
         if str(types[args[0]]) == "<class 'int'>" and str(types[args[-1]]) == "<class 'int'>":
             first = st.number_input(f"Enter Value for {args[0]}: ")
             second = st.number_input(f"Enter Value for {args[-1]}: ")
-            if st.button('Calculate'):
-                result = function(first, second)
-                st.write(f"The result for {metric} is {result}")
+            if second and first:
+                calc = st.button('Calculate')
+                if calc:
+                    result = function(first, second)
+                    st.write(f"The result for {metric} is {result}")
 
         elif str(types[args[0]]) == "<class 'list'>" and str(types[args[-1]]) == "<class 'int'>":
-            n = st.number_input(f"Enter the number of items in {args[0]}")
+            n = int(st.number_input(f"Enter the number of items in {args[0]}"))
             items = []
-            for i in range(n):
-                item = st.number_input(f'Enter item {i+1}')
-                items.append(item)
+            if n:
+                for i in range():
+                    item = st.number_input(f'Enter item {i+1}')
+                    if item:
+                        items.append(item)
 
             second = st.number_input(f"Enter Value for {args[-1]}: ")
-            if st.button('Calculate'):
-                result = function(items, second)
-                st.write(f"The result for {metric} is {result}")
+
+            if items != [] and second:
+                calc = st.button('Calculate')
+                if calc:
+                    result = function(items, second)
+                    st.write(f"The result for {metric} is {result}")
         else:
             st.write('Done')
     else:
@@ -40,18 +47,23 @@ def run_metrics(metric: str):
         for arg in args:
             if str(types[arg]) == "<class 'int'>":
                 first_value = st.number_input(f"Enter value for {arg}")
-                values[arg] = first_value
+                if first_value:
+                    values[arg] = first_value
             else:
-                n = st.number_input(f"Enter the number of items in {arg}")
+                n = int(st.number_input(f"Enter the number of items in {arg}"))
                 second_value = []
-                for i in range(n):
-                    item = st.number_input(f'Enter item {i+1}')
-                    second_value.append(item)
-                    values[arg] = second_value
+                if n:
+                    for i in range(n):
+                        item = st.number_input(f'Enter item {i+1}')
+                        if item:
+                            second_value.append(item)
+                            values[arg] = second_value
 
-        if st.button('Calculate'):
-            result = function(**values)
-            st.write(f"The result for {metric} is {result}")
+        if values != {}:
+            calc = st.button('Calculate')
+            if calc:
+                result = function(**values)
+                st.write(f"The result for {metric} is {result}")
 
 title_alignment= """
 <style>
@@ -64,9 +76,11 @@ st.markdown(title_alignment, unsafe_allow_html=True)
 
 st.title('HELLO!!! YOU CAN TEST EACH OF THE METRICS ON THIS PAGE')
 
-start_button = st.button('Click Here to Test a Metric')
-if start_button:
-    st.write(f"Please enter the metrics you want among these\n {' '.join(funcs)}\nChceck [Docs](https://king-tomi.github.io/metrics/references/) for further information on the functions")
-    
-    metric = st.text_input('What Metric are you looking to measure: ', 'cor')
-    run_metrics(metric)
+st.write(f"Please enter the metrics you want among these\n {' '.join(funcs)}\nChceck [Docs](https://king-tomi.github.io/metrics/references/) for further information on the functions")
+
+metric = st.text_input('What Metric are you looking to measure: ')
+
+if metric:
+    start_button = st.button('Click Here to Test a Metric')
+    if start_button:
+        run_metrics(metric)
